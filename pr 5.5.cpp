3f40ac6468ef9d35f1529048d420e23427f87667
Andrew <90443266+Andrew2865@users.noreply.github.com>
2021-11-17 22:45:04 +0200

    Add files via upload

---
+++ pr 5.5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double S, p, q, S(p, q), f(n), n;
+		cout << "p" << cin >> p;
+	cout << "q" << cin >> q;
+	
+	{
+		cout << "n =" << cin >> n;
+		if (n % 10 > 0)
+			f(n) = n % 10;
+		if (n = 0)
+			f(n) = 0;
+		else
+			f(n) = f(n / 10);
+	}
+
+	{
+		S = 0;
+		S(p, q) = S;
+		if (i = p, i < q, i++)
+			S += f(n);
+		else
+			cout << "S(p, q)" << cin >> S(p, q);
+
+	}
+}
+return 0;
