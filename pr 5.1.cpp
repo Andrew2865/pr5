e174cdb2eef9a536273451a894d5eee12e94710f
Andriy Stogryn <andrewshtogryn@gmail.com>
2021-12-22 20:47:53 +0200

    first master commit

---
+++ pr 5.1.cpp
@@ -0,0 +1,20 @@
+﻿//Лабораторна робота 5.1
+//Штогрина Андрія
+//Варіант 9
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h (const double x, const double y); // прототип
+int main()
+{
+	double t, s;
+		cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = h(1,s+t) + h(s,s-t) - h(t,sqrt((s*s)+(t*t)));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h (const double x, const double y ) // визначення
+{
+	return ((x*x)+(y*y))/((1+(x*x))+((x*x)*(y*y)));
+}
